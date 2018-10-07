23f4d8b2c982dd327cbff7035234191ff3e7f1f2
VJ <vijayasekard.web@gmail.com>
2018-10-07 14:10:22 -0400

    Time zone added to before action in base controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,5 +4,9 @@ class Api::V1::BaseController < Users::SessionsController
   # Doorkeeper code
   before_action :doorkeeper_authorize!
   #respond_to :json
+  before_action :set_user_time_zone
 
+  def set_user_time_zone
+    Time.zone = current_user.time_zone if user_signed_in?
+  end
 end
